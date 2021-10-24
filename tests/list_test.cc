513e10a90668923c4e8242010b2008019c7b7b1d
FengShun <shawnfeng0@gmail.com>
2021-10-25 06:46:00 +0800

    Add list unit test

--- tests/list_test.cc
+++ tests/list_test.cc
@@ -6,24 +6,124 @@
 
 #include <gtest/gtest.h>
 
-#include "list/list.h"
+#include <list>
 
 struct list_test_struct {
   int value;
 
-  list::list_node list1;
-  list::list_node list2;
+  list::list_node node1;
+  list::list_node node2;
 };
 
-TEST(list, test) {
-  //  // create list
-  //  utos::list::list<list_test_struct, &list_test_struct::list1> list1;
-  //
-  //  // insert some elements
-  //  list_test_struct s1, s2, s3, s4;
-  //
-  //  list1.insert_head(&s1);
-  //  list1.insert_head(&s2);
-  //  list1.insert_head(&s3);
-  //  list1.insert_head(&s4);
+TEST(list, push_front) {
+  std::list<list_test_struct> s(10);
+
+  list::list<list_test_struct, &list_test_struct::node1> list;
+  for (auto& i : s) {
+    list.push_front(&i);
+  }
+
+  ASSERT_EQ(list.front(), &s.back());
+  ASSERT_EQ(list.back(), &s.front());
+}
+
+TEST(list, push_back) {
+  std::list<list_test_struct> s(10);
+  list::list<list_test_struct, &list_test_struct::node1> list;
+  for (auto& i : s) {
+    list.push_back(&i);
+  }
+
+  ASSERT_EQ(list.front(), &s.front());
+  ASSERT_EQ(list.back(), &s.back());
+}
+
+TEST(list, pop) {
+  std::list<list_test_struct> s(10);
+  list::list<list_test_struct, &list_test_struct::node1> list;
+  for (auto& i : s) {
+    list.push_back(&i);
+  }
+
+  for (int i = 0; i < 3; ++i) {
+    ASSERT_EQ(list.front(), &s.front());
+    list.pop_front();
+    s.pop_front();
+  }
+
+  for (int i = 0; i < 3; ++i) {
+    ASSERT_EQ(list.back(), &s.back());
+    list.pop_back();
+    s.pop_back();
+  }
+}
+
+TEST(list, empty) {
+  std::list<list_test_struct> s(10);
+  list::list<list_test_struct, &list_test_struct::node1> list;
+  ASSERT_TRUE(list.empty());
+
+  for (auto& i : s) {
+    list.push_back(&i);
+    ASSERT_FALSE(list.empty());
+  }
+
+  for (int i = 0; i < 10; ++i) {
+    ASSERT_FALSE(list.empty());
+    list.pop_front();
+  }
+
+  ASSERT_TRUE(list.empty());
+}
+
+TEST(list, rotate_left) {
+  std::list<list_test_struct> s(10);
+  list::list<list_test_struct, &list_test_struct::node1> list;
+  ASSERT_TRUE(list.empty());
+  for (auto& i : s) {
+    list.push_back(&i);
+  }
+
+  auto front = list.front();
+  list.rotate_left();
+  ASSERT_NE(front, list.front());
+  ASSERT_EQ(front, list.back());
+}
+
+TEST(list, is_singular) {
+  std::array<list_test_struct, 3> s{};
+  list::list<list_test_struct, &list_test_struct::node1> list;
+
+  ASSERT_FALSE(list.is_singular());
+
+  list.push_front(&s[0]);  // 1
+  ASSERT_TRUE(list.is_singular());
+  list.push_front(&s[1]);  // 2
+  ASSERT_FALSE(list.is_singular());
+  list.push_front(&s[2]);  // 3
+
+  ASSERT_FALSE(list.is_singular());
+
+  list.pop_back();  // 2
+  ASSERT_FALSE(list.is_singular());
+  list.pop_back();  // 1
+  ASSERT_TRUE(list.is_singular());
+  list.pop_back();  // 0
+
+  ASSERT_FALSE(list.is_singular());
+}
+
+TEST(list, iterator) {
+  std::list<list_test_struct> s(10);
+  list::list<list_test_struct, &list_test_struct::node1> list;
+
+  for (auto& i : s) {
+    list.push_back(&i);
+  }
+
+  auto i = s.begin();
+  auto j = list.begin();
+  for (; i != s.end() && j != list.end(); ++i, ++j) {
+    ASSERT_EQ(&*i, *j);
+  }
 }
